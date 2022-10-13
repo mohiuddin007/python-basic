252fceb4d2584c60f45474d3d9ed2cf15a096e5b
mohiuddin007 <mohiuddinmazumder94@gmail.com>
2022-10-14 01:07:18 +0600

    wallpaper changed

--- automate-wallpaper/auto_wallpaper.py
+++ automate-wallpaper/auto_wallpaper.py
@@ -3,6 +3,7 @@
 """
 import requests
 import json
+import PyWallpaper
 
 api_url0 = "https://apod.nasa.gov/apod/image/2209/WR140_WebbSchmidt_960.jpg"
 api_url = "https://cdn.searchenginejournal.com/wp-content/uploads/2019/08/c573bf41-6a7c-4927-845c-4ca0260aad6b-760x400.jpeg"
@@ -21,8 +22,11 @@ image_url = dict_content['url']
 res = requests.get(image_url)
 
 #save the image
-with open('apid.png', 'wb') as image:
+with open('apod.png', 'wb') as image:
     image.write(res.content)
+
+#set as wallpaper
+PyWallpaper.change_wallpaper('apod.png')
 
 
 
